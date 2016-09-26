28d6596c0ab1516086b9dc1acfe0cf16c2c0bea5
Mavlarn <mavlarn@liepiaowang.com>
2016-09-26 19:52:03 +0800

    finish example

--- app/template-forms/template-forms.component.ts
+++ app/template-forms/template-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
     selector: 'template-form',
@@ -6,8 +7,11 @@ import { Component } from '@angular/core';
     styleUrls: ['app/template-forms/template-forms.component.css']
 })
 export class TemplateFormsComponent {
-    // user: any;
-        user = {
+    user: any;
+    active: Boolean = true;
+    errorMsg: String;
+    constructor() {
+        this.user = {
             name: '张三',
             mobile: 13800138001,
             address: {
@@ -15,12 +19,29 @@ export class TemplateFormsComponent {
                 street: '朝阳望京...'
             }
         };
-        name = '张三';
-
-    constructor() {
     }
-    logForm(value: any) {
-        console.log(value);
+    logForm(theForm: NgForm) {
+        if (theForm.invalid) {
+            this.errorMsg = 'validation errors!';
+            if (theForm.controls['name'].errors) {
+                this.errorMsg += JSON.stringify(theForm.controls['name'].errors);
+            }
+        }
+        console.log(theForm.value);
+    }
+
+    create() {
+        this.user = {
+            address: {}
+        };
+        this.active = false;
+        setTimeout(() => this.active = true, 0);
+    }
+    reset(theForm: NgForm) {
+        theForm.reset();
+    }
+    diagnostic(model) {
+        console.log(JSON.stringify(model));
     }
  }
 
